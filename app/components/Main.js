a9b6ba7bb046cf4ab7d51a3f4e07299824805fed
Christian M <cmackm@hotmail.com>
2016-09-02 16:36:47 -0600

    Further Router configuration, Route children, Sentry

--- app/components/Main.js
+++ app/components/Main.js
@@ -3,9 +3,9 @@ var React = require('react');
 var Main = React.createClass({
 	render: function () {
 		return (
-			<div> Hello from Main! </div>
+			<div className='main-container'> Hello from Main! {this.props.children} </div>
 			)
-	})
+	}
 });
 
 module.exports = Main;
--- app/config/routes.js
+++ app/config/routes.js
@@ -2,16 +2,19 @@ var React = require('react');
 var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
-var IndexRouter = ReactRouter.IndexRoute;
-var Main = Main = require('../components/Main'); //Routed Components
-var Home = Main = require('../components/Home');
+var hashHistory = ReactRouter.hashHistory;
+var IndexRoute = ReactRouter.IndexRoute;
+var Main = require('../components/Main'); //Routed Components
+var Home = require('../components/Home');
 
 //Routes
 
 var routes = (
-	<Router>
+	<Router history={hashHistory}>
 	  <Route path='/' component={Main}>
-	  	<Route path='/home' component={Home} /> 
+	  	<IndexRoute component={Home} /> 
 	  </Route>
 	</Router>
 	);
+
+module.exports = routes;
