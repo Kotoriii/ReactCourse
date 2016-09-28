c277b2148772d1d48a3ea00c366e4aff5e443180
Christian M <cmackm@hotmail.com>
2016-09-27 18:43:40 -0600

    Start of rendering of the ConfirmBattle component

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,13 +1,47 @@
 var React = require('react');
+var PropTypes = React.propTypes;
+var styles = require('../styles');
 
-function puke (object) {
+/*function puke (object) {
 	return <pre>{JSON.stringify(obj, null, ' ')}</pre>
-}
+}*/
 
 function ConfirmBattle (props) {
 	return props.isLoading === true
 		? <p> LOADING </p>
-		: <div> CONFIRM BATTLE </div>
+		//: <div> CONFIRM BATTLE: {puke(props)} </div>
+		: <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+        <h1>Confirm Players</h1>
+        <div className='col-sm-8 col-sm-offset-2'>
+         <div className='col-sm-6'>
+          <p className='lead'>Player 1</p>
+          Player 1 Info
+         </div>
+         <div className='col-sm-6'>
+          <p className='lead'>Player 2</p>
+          Player 2 Info
+         </div>
+        </div>
+        <div className='col-sm-8 col-sm-offset-2'>
+          <div className='col-sm-12' style={styles.space}>
+            <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>
+            	Initiate Battle!
+            </button>
+          </div>
+          <div className='col-sm-12' style={styles.space}>
+            <Link to='/playerOne'>
+             LINK TO /PLAYERONE 3.50
+            </Link>
+          </div>
+        </div>
+      </div>
+}
+
+ConfirmBattle.propTypes = {
+	isLoading: propTypes.bool.isRequired,
+	onInitiateBattle: PropTypes.func.isRequired,
+	playersInfo: PropTypes.array.isRequired
+
 }
 
 module.exports = ConfirmBattle;
--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -23,9 +23,18 @@ var ConfirmBattleContainer = React.createClass({
           })
         }.bind(this))
     },
+    handleInitiateBattle: function () {
+      this.context.router.push({
+        pathname: '/results',
+        state: {
+          playerInfo: this.state.playersInfo
+        }
+      })
+    },
     render: function(){
     return (
        <ConfirmBattle isLoading={this.state.isLoading}
+        onInitiateBattle={this.handleInitiateBattle}
         playersInfo={this.state.playersInfo} /> 
         );
   }
