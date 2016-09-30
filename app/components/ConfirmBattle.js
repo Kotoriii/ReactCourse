436f44a4a4219208fae132bf40fc88737b6e5ea0
Christian M <cmackm@hotmail.com>
2016-09-29 18:23:43 -0600

    fixed a ConfirmBattleContainer mistake and added more UI rendering for ConfirmBattle

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,7 +1,10 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
+var ReactRouter = require('react-router');
 var styles = require('../styles');
 var Link = require('react-router').Link;
+var UserDetails = require('./UserDetails');
+var UserDetailsWrapper = require('./UserDetailsWrapper');
 
 /*function puke (object) {
 	return <pre>{JSON.stringify(obj, null, ' ')}</pre>
@@ -14,24 +17,20 @@ function ConfirmBattle (props) {
 		: <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
         <h1>Confirm Players</h1>
         <div className='col-sm-8 col-sm-offset-2'>
-         <div className='col-sm-6'>
-          <p className='lead'>Player 1</p>
-          Player 1 Info
-         </div>
-         <div className='col-sm-6'>
-          <p className='lead'>Player 2</p>
-          Player 2 Info
-         </div>
+          <UserDetailsWrapper header='Player 1'>
+            <UserDetails info={props.playersInfo[0]} />
+          </UserDetailsWrapper>
+          <UserDetailsWrapper header='Player 2'>
+            <UserDetails info={props.playersInfo[1]} />
+          </UserDetailsWrapper>
         </div>
         <div className='col-sm-8 col-sm-offset-2'>
           <div className='col-sm-12' style={styles.space}>
-            <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>
-            	Initiate Battle! 5:30
-            </button>
+            <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>Initiate Battle!</button>
           </div>
           <div className='col-sm-12' style={styles.space}>
             <Link to='/playerOne'>
-             <button type='button' className='btn btn-lg btn-danger'>Reselect Players</button>
+              <button type='button' className='btn btn-lg btn-danger'>Reselect Players</button>
             </Link>
           </div>
         </div>
